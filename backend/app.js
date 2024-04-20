f613da7d68392915be9ca6bb347a8077f0def369
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-20 18:58:01 -0300

    form create_user

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.route('/test')
+app.route('/create_user')
     .get((req, res) =>{
-        res.render('test')
+        res.render('create_user')
     })
+    .post((req, res) => {
+      console.log(req.body.username)
+      console.log(req.body.email)
+      console.log(req.body.password)
+      res.send('deu certo');
+    });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
