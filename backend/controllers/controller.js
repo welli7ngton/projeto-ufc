60cba367b46864bd75c60935dad7609a1d7472b7
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-21 13:25:15 -0300

    ajustando ambiente para renderizar os dados nas views

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -2,7 +2,7 @@ import userService from "../services/services.js"
 
 export function getUsers(req, res) {
     const userServices = new userService();
-    console.log(userServices.getAll());
+    // console.log(userServices.getAll());
     res.render("verUsuarios",{
         pageTitle: 'User list',
         users: userServices.getAll()
