a38e7cbf9bb50851a89bac3e6b3d8507728afa57
Pedro Henrique Gomes <pedrohenriquegomeslima1@gmail.com>
2024-05-02 22:31:56 -0300

    adicionando o campo bio na model, refaturando services, controller e views

--- backend/services/services.js
+++ backend/services/services.js
@@ -3,11 +3,11 @@ import userModel from "../models/user.js";
 class userService {
     constructor() {
         this.myUsers = [
-            new userModel("joaozinho", "joao@gmail.com", "235656", 1),
-            new userModel("maria", "maria@gmail.com", "235656", 2),
-            new userModel("gton", "jgton@gmail.com", "235656", 3),
-            new userModel("ueliton", "ueliton@gmail.com", "235656", 4),
-            new userModel("marin", "karin@gmail.com", "235656", 5)
+            new userModel("joaozinho", "joao@gmail.com", "235656","minha bio", 1),
+            new userModel("maria", "maria@gmail.com", "235656","minha bio", 2),
+            new userModel("gton", "jgton@gmail.com", "235656","minha bio", 3),
+            new userModel("ueliton", "ueliton@gmail.com", "235656","minha bio", 4),
+            new userModel("marin", "karin@gmail.com", "235656","minha bio", 5)
         ];
     }
 
@@ -15,9 +15,9 @@ class userService {
         return this.myUsers;
     }
 
-    createUser(userName, email, password) {
+    createUser(userName, email, password, bio) {
         const nextId = this.myUsers.length + 1;
-        return  this.myUsers.push(new userModel(userName, email, password, nextId));
+        return  this.myUsers.push(new userModel(userName, email, password, bio, nextId));
     }
 
     deleteUser(id) {
@@ -34,11 +34,12 @@ class userService {
         return false
     }
 
-    updateProfile(id, username, email) {
+    updateProfile(id, username, email, bio) {
         const userIndex = this.myUsers.findIndex(user => user.id === parseInt(id));
         if (userIndex !== -1) {
             this.myUsers[userIndex].userName = username;
             this.myUsers[userIndex].email = email;
+            this.myUsers[userIndex].bio = bio;
             return "Usuário alterado"
         } else {
             return "Usuário não encontrado"
