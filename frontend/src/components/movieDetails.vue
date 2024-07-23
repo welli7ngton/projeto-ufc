a19dd89e01f6139cca1aaea22b92a94c431dfa97
Pedro H. Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2024-07-22 21:13:30 -0300

    refatorando o post axios e o alert com a lib sweetAlert

--- frontend/src/components/movieDetails.vue
+++ frontend/src/components/movieDetails.vue
@@ -4,6 +4,7 @@
 
 <script>
 import axios from 'axios';
+import Swal from 'sweetalert2'
 
 export default {
   name: 'MovieDetails',
@@ -64,37 +65,26 @@ export default {
         const movieData = await movieResponse.json();
         const creditsData = await creditsResponse.json();
         
-        // TODO: refatorar e remover essa repetição de código, muitos atributos repetidos
-        this.movieDetails = {
-          idTMDB: this.movieId || '',
-          title: movieData.title || '',
-          plot: movieData.overview || '',
-          released: movieData.release_date || '',
-          runtime: movieData.runtime || '',
-          genre: movieData.genres ? movieData.genres.map(genre => genre.name).join(', ') : '',
-          director: this.getDirector(creditsData.crew) || 'Unknown',
-          writer: this.getWriter(creditsData.crew) || 'Unknown',
-          country: movieData.production_countries ? movieData.production_countries.map(country => country.name).join(', ') : '',
-          awards: movieData.awards || '',
-          imdbRating: movieData.vote_average || ''
-        };
-
-        if (this.movieDetails.title !== "") {
+        if ( movieData.title !== "") {
           axios.post('http://localhost:3000/movies/createMovie', {
-            idTMDB: this.movieDetails.idTMDB,
-            title: this.movieDetails.title,
-            plot: this.movieDetails.plot,
-            released: this.movieDetails.released,
-            runtime: this.movieDetails.runtime,
-            gender: this.movieDetails.genre,
-            director: this.movieDetails.director,
-            writer: this.movieDetails.writer,
-            country: this.movieDetails.country,
-            imdbRating: this.movieDetails.imdbRating
+            idTMDB: this.movieId || '',
+            title: movieData.title || '',
+            plot: movieData.overview || '',
+            released: movieData.release_date || '',
+            runtime: movieData.runtime || '',
+            gender: movieData.genres ? movieData.genres.map(genre => genre.name).join(', ') : '',
+            director: this.getDirector(creditsData.crew) || 'Unknown',
+            writer: this.getWriter(creditsData.crew) || 'Unknown',
+            country: movieData.production_countries ? movieData.production_countries.map(country => country.name).join(', ') : '',
+            imdbRating: movieData.vote_average || ''
           }, { timeout: 10000 })
-            .then(movieResponse => {
-              console.log(movieResponse.statusText)
-              // TODO: criar spam ou arranjar qualquer forma de mostrar uma mensagem de sucesso ao cadastrar um filme.
+            .then(()=> {
+              Swal.fire({
+              icon: 'success',
+              title: 'Sucesso!',
+              text: 'Filme adicionado com sucesso'
+              //TODO adicionar o style
+            });
             })
             .catch(error => {
               console.error('Erro ao adicionar filme:', error);
